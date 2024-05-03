fb64d5cb92db3a3762eb0ccb8c01fc6112e67b8e
navegantes <raphaelnavegantes@gmail.com>
2024-05-03 17:36:05 -0300

    add helloworld route and test

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,9 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/helloworld', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def html_response():
+    html_resp = """<h1>Olá Mundo!</h1>"""
+    return HTMLResponse(content=html_resp)
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,22 @@ from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
+client = TestClient(app)  # Arrange
+
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
+    # client = TestClient(app)  # Arrange
 
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_html_message_ola_mundo():
+    # client = TestClient(app)  # Arrange
+
+    response = client.get('/helloworld')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.text == """<h1>Olá Mundo!</h1>"""  # Assert
